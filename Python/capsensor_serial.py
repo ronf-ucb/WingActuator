c8dc7dc1b2200b4cb337efaa36330de61b5f26f8
ronf-ub <ronf@berkeley.edu>
2019-07-28 15:26:22 -0700

    python reads frame and substracts offset

--- Python/capsensor_serial.py
+++ Python/capsensor_serial.py
@@ -11,7 +11,7 @@ import serial
 
 offset_array = np.zeros((8,8))  # offset for unloaded sensor
 sensor_array = np.zeros((8,8))
-
+offset_flag = False
 
 
 BS_COMPORT = 'COM3' # ron
@@ -25,35 +25,73 @@ numSamples = 3000 # 1 kHz sampling in pid loop = 3 sec
 alldata = [ [] ] * (numSamples+1)
 
 
-ser = serial.Serial(BS_COMPORT, BS_BAUDRATE, timeout=10) 
+ser = serial.Serial(BS_COMPORT, BS_BAUDRATE, timeout=30) 
 
 def read_frame():
+    temp_array = np.zeros((8,8))
     ser.reset_input_buffer() # discard if current content in buffer
-    ser.read_until('F',1)   # wait for first character of frame timing
+    ser.read_until('Fr',2)   # wait for first character of frame timing
     first_line = ser.readline()
+#    print('First line',first_line)
     timestamp = float(first_line.split()[2]) # get time of sample
     print('Time stamp',timestamp)
-    for i in range(0,7):
+    for i in range(0,8):
         line = ser.readline()
         line_array = np.array(list(map(float, line.split())))
- #       print(line,line_array)
-        sensor_array[i] = line_array
+  #     print(line,line_array)
+        temp_array[i] = line_array
  #       pdb.set_trace() 
  #   print(sensor_array)
-    return sensor_array
+    ser.flush()  # clear out left over characters
+    return temp_array
+ 
+def menu():
+    print("-----------------enter char + <cr> --------------------")
+    print("s: read sensor        |o: calculate offset")
+    print("q: quit        |")
     
-def main():
-    # 1 second timeout
-    print('Sensor Interface to Cap sensor running on K64F')
+     
+def display_sensor():
+    print('second read is array value')
+    temp_array = read_frame()
+    sensor_array = temp_array - offset_array
+    if offset_flag:
+        print('Raw - Offset array\n', sensor_array)
+    else:
+        print('need offset first')
+
+
+def read_offset():
+    global offset_flag
     print('First read is offset')
     offset_array = read_frame()
     print('Offset array\n', offset_array)
-    print('second read is array value')
-    sensor_array = read_frame()
-    print('Offset array\n', sensor_array)
-#    print('Reading serial port')
-#    s = ser.read(200)   
-#    print(s)
+    offset_flag = True
+   
+def main():
+    # 1 second timeout
+    print('Sensor Interface to Cap sensor running on K64F')
+   #blank out any keypresses leading in...
+#    while msvcrt.kbhit():
+#        ch = msvcrt.getch()
+    menu()
+    while True:
+        print('>',)
+       # keypress = msvcrt.getch()   
+        keypress = input()
+        if keypress == '?':
+            menu()
+        elif keypress == 'o':
+            read_offset()
+        elif keypress == 's':
+            display_sensor()
+        elif (keypress == 'q') or (ord(keypress) == 26):
+            print("Exit.")
+            ser.close()
+            sys.exit(0)
+        else:
+            print("** unknown keyboard command** \n")
+            menu()    
     ser.close() # close port so won't conflict with next read 
 
 
