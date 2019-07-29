327e0042591e5bc70721513f415eaee5828a18b7
ronf-ub <ronf@berkeley.edu>
2019-07-28 18:46:09 -0700

    added gray scale plot (but won't show until exit)

--- Python/plot-test.py
+++ Python/plot-test.py
@@ -1,50 +1,56 @@
-# Cap sensor interface. K64F running 8x8 capacitive sensor
-# R.Fearing July 2019
-############# plot won't show until end #####
-#import msvcrt, sys
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 28 17:40:10 2019
 
+@author: ronf
+"""
+# test gray scale plot
 import matplotlib.pyplot as plt
-import numpy as np # after matplotlib???
+import numpy as np
 import sys
-#from struct import *
-#import time
-#import pdb
 import serial
-
+#import pdb
 plt.close("all")   # try to close all open figs
 
 offset_array = np.zeros((8,8))  # offset for unloaded sensor
 sensor_array = np.zeros((8,8))
 offset_flag = False
 
-
 BS_COMPORT = 'COM3' # ron
 # BS_COMPORT = 'COM4' # usually com3 or com4 depending on computer
 # check Ports under device manager afetr plugging in K64F
 BS_BAUDRATE = 115200
+ser = serial.Serial(BS_COMPORT, BS_BAUDRATE, timeout=30) 
 
 
 data_file_name = 'imudata.txt'
 numSamples = 3000 # 1 kHz sampling in pid loop = 3 sec
 alldata = [ [] ] * (numSamples+1)
 
-ser = serial.Serial(BS_COMPORT, BS_BAUDRATE, timeout=30) 
+
+
+
+
+
+
+
+
+
 
 INTENSITY_MAX = 255
 INTENSITY_MIN = 0
 
-
 def plot_array():
-    fig, ax = plt.subplots(1, 1)  # create just one plot
     Z = np.random.rand(8, 8)
+    fig, ax = plt.subplots(1, 1)
     c = ax.pcolor(Z, cmap='Greys')
-    ax.set_title('Cap sensor')
+    ax.set_title('inside plot_array')
     fig.tight_layout()
-    plt.show()
+    plt.show(block=False)  # this blocks on user input
+    plt.pause(0.1)          # The plot properly appears.
     
     
-
-
+    
 def read_frame():
     temp_array = np.zeros((8,8))
     ser.reset_input_buffer() # discard if current content in buffer
@@ -87,39 +93,30 @@ def read_offset():
     offset_array = read_frame()
     print('Offset array\n', offset_array)
     offset_flag = True
-   
+       
+    
+ 
+    
+    
 def main():
     # 1 second timeout
     print('Sensor Interface to Cap sensor running on K64F')
-    print("Note: Will not generate plot until exiting program. matplotlib bug")
    #blank out any keypresses leading in...
 #    while msvcrt.kbhit():
 #        ch = msvcrt.getch()
- #   pdb.set_trace()
+#    pdb.set_trace()
     menu()
-    while True:
-        print('>',)
-       # keypress = msvcrt.getch()   
-        keypress = input()
-        if keypress == '?':
-            menu()
-        elif keypress == 'o':
-            read_offset()
-        elif keypress == 's':
-            display_sensor()
-        elif keypress == 'p':
-            plot_array()
-        elif (keypress == 'q') or (ord(keypress) == 26):
-            print("Exit.")
-            ser.close()
-            sys.exit(0)
-        else:
-            print("** unknown keyboard command** \n")
-            menu()    
+    keypress = 'x'
+    keypress = input()
+    print('keypress=',keypress)
+    plot_array()
+    keypress = input()
+    print('keypress=',keypress)
+    plot_array()
     ser.close() # close port so won't conflict with next read 
 
-
-#Provide a try-except over the whole main function
+    
+    #Provide a try-except over the whole main function
 # for clean exit. The Xbee module should have better
 # provisions for handling a clean exit, but it doesn't.
 if __name__ == '__main__':
@@ -133,7 +130,3 @@ if __name__ == '__main__':
         print(inst) 
         print("IO Error.")
         ser.close()
-
-
-
-
